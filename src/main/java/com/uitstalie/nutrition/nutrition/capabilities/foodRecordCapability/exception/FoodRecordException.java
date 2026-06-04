2cf91d9b91aecbacc93c5fd46df990a5e1dba7dd
uitstalie <daiqiancheng0924@126.com>
2026-06-04 11:06:38 +0800

    cleanup: remove 110 old namespace files, update .gitignore

--- src/main/java/com/uitstalie/nutrition/nutrition/capabilities/foodRecordCapability/exception/FoodRecordException.java
+++
@@ -1,7 +0,0 @@
-package com.uitstalie.nutrition.nutrition.capabilities.foodRecordCapability.exception;
-
-public class FoodRecordException extends Exception {
-    public FoodRecordException(String message) {
-        super(message);
-    }
-}
